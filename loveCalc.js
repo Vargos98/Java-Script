29ea9f3f427a440565255bd96f1d7b1b3799ca96
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-06-05 13:22:06 +0530

    LoveCalc& randomNoGenerator

---
+++ loveCalc.js
@@ -0,0 +1,8 @@
+// var lover1 = prompt("Enter your 1st lovers name: ");
+// var lover2 = prompt("Enter your 2nd lovers name: ");
+
+var n = Math.random();
+n = n *100;
+var percentage = Math.floor(n) + 1;
+
+console.log("Your love percentage is : "+percentage+ " %");
---
+++ randomNoGenerator.js
@@ -0,0 +1,5 @@
+var n = Math.random();
+// console.log(n);
+n = n *6;
+var round = Math.floor(n) + 1  ; // added +1 so that the number never goes below and stays in range of 1 and 6;
+console.log(round);
