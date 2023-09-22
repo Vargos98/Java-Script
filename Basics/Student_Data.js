c47fe0f699d62f8293f330959ce8467e715c3e66
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-22 09:23:01 +0530

    Started JavaScript fundamentals part-1

---
+++ Basics/Student_Data.js
@@ -0,0 +1,22 @@
+const data = {
+    name:"Umesh Kumar",
+    phone: "9828206117",
+    id:"207btechcse008"
+}
+console.log(data)
+
+
+// q1
+
+let a = "umesh"
+let b="9"
+console.log(a+b)
+ 
+// q2
+
+const ba = {
+    ename:"umesh",
+    bnum: "829",
+
+}
+console.log(typeof(ename))
