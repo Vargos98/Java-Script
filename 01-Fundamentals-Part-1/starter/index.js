9b9f089417e525013ed1851aa82242b53e6daca5
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-22 11:29:55 +0530

    Update index.js

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -13,6 +13,25 @@
 // console.log(typeof null)
 
 
+
+
+
+// const firstName = "Umesh";
+// const birthYear = 1998;
+// const job = "developer";
+// const year = 2023;
+
+// // this method is the basic way of writing a String.
+// const out = "I'm" + firstName + "i am a " + job + "my age is " + (year - birthYear);
+// console.log(out);
+
+// // Best way of writing a string
+
+// const newOut = `I'm ${firstName}, i am a ${job}, my age is ${year- birthYear}, years old `;
+// console.log(newOut)
+  
+
+
 const massMark = 78;
 const heightMark = 1.69;
 
@@ -23,8 +42,12 @@ const BMIMark = massMark / heightMark ** 2;
 
 const BMIJohn = massJohn / heighJohn ** 2;
 
-console.log(BMIMark);
-console.log(BMIJohn);
+console.log(BMIMark , BMIJohn);
 
-console.log( BMIMark < BMIJohn);
 
+if(BMIMark > BMIJohn){
+    console.log(`Marks BMI ${BMIMark}, is higher than John's ${BMIJohn}, !`);
+    
+}else{
+    console.log(`John's BMI ${BMIJohn}, is higher than Mark's ${BMIMark}, !`);
+}
