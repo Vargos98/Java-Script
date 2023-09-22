3a3b2dba1a8d88ef55f6e1925b2d5224420ac53a
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-22 09:53:00 +0530

    Learnt naming conventions

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,11 @@
+
+let js= "amazing";
+
+if(js === "amazing")alert("Javascript loaded")
+
+
+let country = "India";
+let continent = " Asia";
+let population = " 7 millions";
+
+console.log(country + continent + population);
