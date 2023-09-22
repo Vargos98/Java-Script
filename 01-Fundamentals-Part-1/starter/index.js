7f2eecebd7202f1517f9d277d2ca02d7e353b7b7
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-22 10:23:24 +0530

    Update index.js
    did a code to calculate BMI

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -1,11 +1,30 @@
 
-let js= "amazing";
+// const js= "amazing";
 
-if(js === "amazing")alert("Javascript loaded")
+// if(js === "amazing")alert("Javascript loaded")
 
 
-let country = "India";
-let continent = " Asia";
-let population = " 7 millions";
+// const country = "India";
+// const continent = " Asia";
+// const population = " 7 millions"; 
 
-console.log(country + continent + population);
+// console.log( country + continent + population);
+// console.log(typeof population)
+// console.log(typeof null)
+
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heighJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+
+const BMIJohn = massJohn / heighJohn ** 2;
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+console.log( BMIMark < BMIJohn);
+
