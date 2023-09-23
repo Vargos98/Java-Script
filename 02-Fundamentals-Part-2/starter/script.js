bc12c85f9375bd43cd0eafae1b9fc3ff12dbdbe1
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-23 10:14:24 +0530

    Started fundamentals part 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,22 @@
+// let hasDriversLicense = false;
+
+// const testPassed = true;
+
+// if(testPassed === true){
+//     hasDriversLicense = true;
+//     console.log(hasDriversLicense);
+// }
+
+
+// function logger(){
+//     console.log("log")
+
+
+// }
+
+// logger();
+// logger();
+// logger();
+// logger();
+
+
