36bf0a235b3f6ea01231b7f756d0461fde63a908
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-23 11:08:42 +0530

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,3 +20,20 @@
 // logger();
 
 
+// function fruitProcessor(apples, oranges){
+//     console.log(apples, oranges); 
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges are very tasty`;
+//     return juice;
+// }
+
+// const juiceReturns = fruitProcessor(5, 4);
+// console.log(juiceReturns);
+
+// console.log(fruitProcessor(8,9));
+
+// function calcAge(year){
+//     const presentYear = 2023;
+//     return(`you are ${presentYear - year} years old`);
+// }
+
+// console.log(calcAge(1998));
