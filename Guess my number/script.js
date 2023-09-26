f073af1e830e33cd1b68f53017e15f0bac4bd4d2
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-26 08:42:51 +0530

    Started a guess number game

---
+++ Guess my number/script.js
@@ -0,0 +1,8 @@
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent="Correct Number !"; 
+
+document.querySelector('.check').addEventListener('click', function(){
+    console.log(document.querySelector('.guess').value);
+});
