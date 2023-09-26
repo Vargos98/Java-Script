a0ab5bc678a6077ff9ee80e67174241e6816e4e6
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-26 09:16:47 +0530

    Added comparison conditions for the data entry

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -3,6 +3,26 @@
 
 // document.querySelector('.message').textContent="Correct Number !"; 
 
+
+const secretNumber = Math.trunc(Math.random()*20 )+1;
+//             Math.trunc = "To remove decimals"
+//                        Math.random() to generate random numbers
+//                                     *20 to range from 0-20 
+//                                          +1 to range from 1-20;
+document.querySelector('.number').textContent=secretNumber;
+
 document.querySelector('.check').addEventListener('click', function(){
-    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+    if(!guess){
+        document.querySelector('.message').textContent="No input!";
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent="You win!";
+
+    }else if(guess < secretNumber){
+        document.querySelector('.message').textContent="Too Low!";
+    }else if(guess > secretNumber){
+        document.querySelector('.message').textContent="Too High!";
+    }
+
+    
 });
