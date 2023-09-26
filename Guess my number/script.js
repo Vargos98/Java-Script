868194a9af048a9f7b3ea4034757cc19aa83ab7a
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-09-26 10:39:29 +0530

    Added reset functionality

--- Guess my number/script.js
+++ Guess my number/script.js
@@ -4,7 +4,7 @@
 // document.querySelector('.message').textContent="Correct Number !"; 
 
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 //             Math.trunc = "To remove decimals"
 //                        Math.random() to generate random numbers
 //                                     *20 to range from 0-20 
@@ -13,35 +13,69 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+
+// again button 
+
+document.querySelector('.again').addEventListener('click', function(){
+    score=20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent="Start guessing...";
+    document.querySelector('body').style.backgroundColor="#222";
+    document.querySelector('.number').textContent="?";
+    document.querySelector('.score').textContent=score;
+    document.querySelector('.guess').value= ' ';
+})
+
 
 document.querySelector('.check').addEventListener('click', function () {
-    
+
     const guess = Number(document.querySelector('.guess').value);
-    
+
     if (!guess) {
-    
+
         document.querySelector('.message').textContent = "No input!";
-    
+
     } else if (guess === secretNumber) {
 
         document.querySelector('.message').textContent = "You win!";
 
+        // display the guessed number.
+        document.querySelector('.number').textContent = secretNumber;
+        
+        
+        // changing background
+        document.querySelector('body').style.backgroundColor="#60b347";
+        document.querySelector('.number').style.width="30rem";
+
 
     } else if (guess < secretNumber) {
-    
-        document.querySelector('.message').textContent = "Too Low!";
-        score = score - 1
-        document.querySelector('.score').textContent = score;
-    
-    } else if (guess > secretNumber) {
-    
-        document.querySelector('.message').textContent = "Too High!";
-        score = score - 1;
-        document.querySelector('.score').textContent = score;
-    
-    }
+        if (score > 1) {
+            
+            document.querySelector('.message').textContent = "Too low!";
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
 
+        }else{
+            document.querySelector('.message').textContent = "You Lost the game :<";
+            document.querySelector('.score').textContent = 0;
+        }
+
+        
+    }else if (guess > secretNumber) {
+        if (score > 1) {
+            
+            document.querySelector('.message').textContent = "Too High!";
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+
+        }else{
+            document.querySelector('.message').textContent = "You Lost the game :<";
+            document.querySelector('.score').textContent = 0;
+        }
+
+        
+    }
 
 });
 
