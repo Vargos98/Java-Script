312d32132f727c24740134985c9e70600bd66ef9
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2023-12-28 21:46:02 +0530

    Update PrintEvenInArr.js

--- Basics/PrintEvenInArr.js
+++ Basics/PrintEvenInArr.js
@@ -1,6 +1,12 @@
 const ages = [ 21,32,11,14,16,17,19,23,27];
 for(let i=0;i<ages.length;i++){
-    if(ages[i]%2==0){
+    if(ages[i]%2==0)
+    
+    {
+ 
+ 
         console.log(ages[i]);
+ 
+ 
     }
 }
