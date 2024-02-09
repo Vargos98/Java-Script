3900d36459f992e97cc6081231e6930f93da7fec
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-09 09:37:26 +0530

    Update Greeter.js

--- Basics/Greeter.js
+++ Basics/Greeter.js
@@ -3,3 +3,4 @@ const greet = greeting => name => console.log(`${greeting}  ${name}`);
 greet('hi')('Umesh');
 // how to return a function 
 // over here greeting is the argument of a function and name is also an argument.   
+
