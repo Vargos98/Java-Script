b6b32c7ccf947bd66e91816e368d60c93433980e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-10 10:46:31 +0530

    practiced few programming logics

---
+++ Basics/SwitchFruits.js
@@ -0,0 +1,15 @@
+let fruit = "Apple";
+
+switch(fruit){
+  case "Banana":
+  console.log("Banana is good!")
+  break
+  case "Orange":
+  console.log("I am not a fan of Orange!")
+  break
+  case "Apple":
+  console.log("How you like them Apples?")
+  break
+  default:
+  console.log("I have never heard of that fruit")
+}
