f2106a74b411bebe84fe412253a762ac5889108e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-10 10:38:49 +0530

    Create Password.js

---
+++ Basics/Password.js
@@ -0,0 +1,13 @@
+ let password = NaN;
+
+ if(password === 8){
+   console.log("Welcome");
+
+ }else if(password<=8){
+  console.log("Password to short");
+ }else if(password>=8){
+  console.log("Too Long Password");
+  console.log("Password should be 8 characters");
+ }else{
+  console.log("Please provide a password")
+ }
