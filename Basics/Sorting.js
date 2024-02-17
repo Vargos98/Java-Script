0f9614814933139a4282a73c6037903829493ca4
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-17 12:24:41 +0530

    Update Sorting.js

--- Basics/Sorting.js
+++ Basics/Sorting.js
@@ -1,7 +1,11 @@
 let arr = [1,2,3,4,5,6]
 
 // console.log(arr.sort());
-
+let slice = arr.slice(1,6);
 let splice = arr.splice(1,4);
+
+console.log(slice);
+
 console.log(splice);
+
 console.log(arr)
