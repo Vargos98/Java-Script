c0312a6c2cf84a08ecb3c754134c8d4be72d345f
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-29 10:07:25 +0530

    Create Favnum.js

---
+++ Revision/Favnum.js
@@ -0,0 +1,8 @@
+const firstFavNum = 13;
+const secondfFavNum = 31;
+
+console.log(firstFavNum + secondfFavNum);
+console.log(firstFavNum * secondfFavNum);
+console.log(firstFavNum - secondfFavNum);
+console.log(firstFavNum / secondfFavNum);
+console.log(firstFavNum % secondfFavNum);
