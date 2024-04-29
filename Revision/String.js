d4cd08fb959a1af71093b6953487f9d483d3215e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-29 10:23:46 +0530

    Created Boolean.js and String.js

---
+++ Revision/String.js
@@ -0,0 +1,15 @@
+let fname = "Umesh";
+let lname = "Kumar";
+
+let fullName = `${fname} ${lname}`;
+
+console.log(fullName);
+
+let capitilize = fullName.toUpperCase();
+console.log(capitilize);
+
+let msg = `My favorite dev is ${fullName} & he's ${capitilize} is cool!`;
+console.log(msg)
+
+msg += ` and he works as a dev!`
+console.log(msg);
