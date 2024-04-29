16e2b59db5a7d5164b77dc2855cdc5a9df92faa7
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-29 11:16:39 +0530

    Create Array.js

---
+++ Revision/Array.js
@@ -0,0 +1,9 @@
+const favSingers = ["demi", "Imagine Dragons", "NF" ];
+console.log(favSingers[0]);
+
+const favNumbers = [12,31,29,26];
+console.log(favNumbers);
+
+const mixedArr = ["Umesh",["Vargos", "EkThaTaxi", "ShortbustGangster"], 123, true];
+console.log(mixedArr)
+console.log(favSingers[2]);
