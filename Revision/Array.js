662814b17fa6dfaf3173f6c43e406c9324e31536
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-29 11:22:52 +0530

    Update Array.js

--- Revision/Array.js
+++ Revision/Array.js
@@ -7,3 +7,7 @@ console.log(favNumbers);
 const mixedArr = ["Umesh",["Vargos", "EkThaTaxi", "ShortbustGangster"], 123, true];
 console.log(mixedArr)
 console.log(favSingers[2]);
+
+
+console.log(favNumbers.slice(1,3))
+console.log(favNumbers.sort())
