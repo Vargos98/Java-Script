558c90f1375edc6edf26eb693462c7fcf5495550
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-30 10:53:29 +0530

    Create ArrowFun.js

---
+++ Revision/Intermediate/ArrowFun.js
@@ -0,0 +1,25 @@
+// Normal function expression 
+
+function greet(userName){
+  console.log(`Hey there ${userName}`)
+}
+
+greet("Vargos");
+
+// Arrow function 
+
+const greetings = (name) =>{
+  return (`How are you ${name}`)
+}
+
+console.log(greetings("Vargos"));
+
+// we can also use arrow functions like this as well for single parameters  
+// But when we have more than one parameter use () parenthesis.
+// also no need to curly brackets and return keyword when we have single line of code.
+
+
+const aloha = surname => (`Your surname is ${surname}`);
+
+
+console.log(aloha("EkThaTaxi"));
