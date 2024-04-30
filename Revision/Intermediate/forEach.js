9c7c83eb5258f199176efde10205b3f836a8032e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-30 13:52:41 +0530

    Update forEach.js

--- Revision/Intermediate/forEach.js
+++ Revision/Intermediate/forEach.js
@@ -1,3 +1,23 @@
 // When we call the forEach we pass in anonymous callback function,
 // this function gets called one time for every element in the array 
 // whatever is inside the function logic will execute.
+
+
+// const nums = [1,2,3,4,12411,123124,12412433546,768678]
+
+// nums.forEach((num)=>{console.log(num)});
+            // ^ CallBack Function
+
+
+//Challenge
+
+
+const numbers = [12,31,43,67,23,98,20]
+let sum =0;
+
+function adder(number){
+  sum+=number
+}
+
+numbers.forEach(adder)
+console.log(sum)
