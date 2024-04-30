3fbe4f46fd51459fd2fab40f08e939a581a388b0
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-04-30 10:58:06 +0530

    Update ArrowFun.js
    Completed a challenge

--- Revision/Intermediate/ArrowFun.js
+++ Revision/Intermediate/ArrowFun.js
@@ -27,3 +27,23 @@ console.log(aloha("EkThaTaxi"));
 const double = n => n*2;
 
 console.log(double(5));
+
+
+//challenge 
+
+
+setTimeout(()=>{
+  console.log("Hello");
+  setTimeout(()=>{
+    console.log("Hey");
+    setTimeout(()=>{
+      console.log("Namaste");
+      setTimeout(()=>{
+        console.log("Hi");
+        setTimeout(()=>{
+          console.log("Bonjour");
+        },2000);
+      },2000)
+    },2000)
+  },2000)
+},2000)
