4f9c103b918437b0eb9e76f7ec19a4b2c57495ac
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-05-03 13:46:53 +0530

    Update ConstructorFunction.js

--- Revision/Advance/ConstructorFunction.js
+++ Revision/Advance/ConstructorFunction.js
@@ -29,11 +29,11 @@ function Person(name,age,gender)
 {
   this.name = name
   this.age = age
-  this.gender = gender
+  this.gender = gender 
 
   this.info = function(){
    console.log(   `${this.name} ${this.age} ${this.gender}`)
-  }
+  } 
 }
 
 const peep1 = new Person("Vargos",26,"M");
