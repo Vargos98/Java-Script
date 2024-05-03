80d952396226b60881867c0aa69b0f84d370c30e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-05-03 12:49:44 +0530

    Update FactoryFunction.js

--- Revision/Advance/FactoryFunction.js
+++ Revision/Advance/FactoryFunction.js
@@ -1,3 +1,6 @@
+// Factory functions are methods through which we can define a set of parameters and we can use those parameters to enter the details/values based on our requirements. In other words these are like parameterized functions used for specific problems.
+
+
 function vehicles(type, brand, model, year){
   return {type,
     brand,
