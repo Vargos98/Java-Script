fa78d7f4ed6a317c05d7b8601c05de378e669cf5
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-05-06 11:00:53 +0530

    Create ThenCatch.js

---
+++ Revision/Advance/ThenCatch.js
@@ -0,0 +1,17 @@
+const getPromise =()=>{
+  return new Promise((resolve,reject)=>{
+    console.log("I am a promise");
+    resolve("success");
+    // reject("error");
+  });
+}
+
+let promise  = getPromise();
+promise.then((res)=>{
+  console.log("promise fulfilled", res);
+});
+
+
+promise.catch((err)=>{
+  console.log("error code :",err);
+})
