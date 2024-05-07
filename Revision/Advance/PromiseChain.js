be38a128569d7468f07e1286ca5fa398382d1ef8
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-05-07 11:13:26 +0530

    Update PromiseChain.js

--- Revision/Advance/PromiseChain.js
+++ Revision/Advance/PromiseChain.js
@@ -1,30 +1,30 @@
-function asyncFunc1() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Some data1");
-      resolve("success");
-    }, 4000);
-  });
+// function asyncFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Some data1");
+//       resolve("success");
+//     }, 4000);
+//   });
 
-}
-function asyncFunc2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Some data2");
-      resolve("success");
-    }, 4000);
-  });
-}
+// }
+// function asyncFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Some data2");
+//       resolve("success");
+//     }, 4000);
+//   });
+// }
 
 
 // let p1 = asyncFunc1();
 
 
-console.log("fetching data1.....")
-asyncFunc1().then((res)=>{                                // example of promise chaining 
-  console.log("fetching data2....");                      // as soon as promise 1 is fulfilled
-  asyncFunc2().then((res)=>{});                           // promise runs.
-});
+// console.log("fetching data1.....")
+// asyncFunc1().then((res)=>{                                // example of promise chaining 
+//   console.log("fetching data2....");                      // as soon as promise 1 is fulfilled
+//   asyncFunc2().then((res)=>{});                           // promise runs.
+// });
 
 
 // p1.then((res) => {
@@ -49,21 +49,72 @@ asyncFunc1().then((res)=>{                                // example of promise 
 //   console.log("Error occured",err);
 // })
 
+// example 4
 
 
+// let ans = new Promise((res,rej)=>{
+//   let n = Math.floor(Math.random()*10);
+//   if(n<5){
+//     return res();
+//   }else{
+//     return rej();
+//   }
+// })
 
-let ans = new Promise((res,rej)=>{
-  let n = Math.floor(Math.random()*10);
-  if(n<5){
-    return res();
+// ans.then(()=>{
+//   console.log("Below");
+// })
+// .catch(()=>{
+//   console.log("Above");
+// })
+
+// example for promise 
+
+let dishReady = new Promise((res,rej)=>{
+  let dish = cookDish();
+  if(dish){
+    res(dish);
   }else{
-    return rej();
+    rej(dish);
   }
 })
 
-ans.then(()=>{
-  console.log("Below");
+
+.then((dish)=>{
+  console.log("Yay! I got my dish", dish);
+  return orderDessert();
 })
-.catch(()=>{
-  console.log("Above");
+.then((dessert)=>{
+  console.log("Dessert is here:", dessert);
 })
+
+.catch((err)=>{
+  console.log('Oh no there was a problem', err);
+})
+
+
+
+
+
+// example 5  for promise chaining
+
+
+let firstTask = new Promise((res,rej)=>{
+  setTimeout(()=>{
+    console.log("First task is completed")
+    res();
+  },2000);
+})
+
+let secondTask = new Promise((res,rej)=>{
+  setTimeout(()=>{
+    console.log("Second task is completed")
+    res();
+  },3000);
+})
+
+firstTask.then(()=>{
+  return secondTask;
+}).then(()=>{
+  console.log("Both tasks are completed");
+});
