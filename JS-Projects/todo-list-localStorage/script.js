73c4cb0699bc02e5e3cdc8b3081141258010d4e2
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-20 11:59:21 +0530

    Update script.js
    Wrapped the whole code in a eventListener so that when the page loads the data is fetched from the localstorage

--- JS-Projects/todo-list-localStorage/script.js
+++ JS-Projects/todo-list-localStorage/script.js
@@ -1,26 +1,40 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener('DOMContentLoaded', () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
 
-let tasks = [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-addTaskButton.addEventListener("click", ()=> {
-  const taskText = todoInput.value.trim();
-  if(taskText === ""){
-    return;
+  // looping through the array
+
+  tasks.forEach(task => {
+
+    renderTask(task);
+    
+  });
+
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") {
+      return;
+    }
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false
+    }
+    tasks.push(newTask)
+    saveTasks();
+    todoInput.value = "";
+    console.log(tasks)//clearing the input value after the task has been submitted.
+  });
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    completed:false
-  }
-  tasks.push(newTask)
-  saveTasks();
-  todoInput.value = ""; 
-  console.log(tasks)//clearing the input value after the task has been submitted.
-});
 
-function saveTasks(){
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+  function renderTask(task) {
+
+  }
+})
