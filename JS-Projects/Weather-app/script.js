f5cdfdf5a0a01a306c03a1d32a5af2397653e577
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-21 12:20:35 +0530

    Update script.js

--- JS-Projects/Weather-app/script.js
+++ JS-Projects/Weather-app/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const temperatureDisplay = document.getElementById('temperature')
   const descriptionDisplay = document.getElementById('description')
   const errorMessage = document.getElementById('error-message');
-  const apiKEY = b0fce0afe9855f29e20ad114ba17b371
+  const API_KEY = b0fce0afe9855f29e20ad114ba17b371
 
   getWeatherBtn.addEventListener('click',async()=>{
     const city = cityInput.value.trim();
@@ -25,7 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
   
   })  
   async function fetchWeatherData(city){
-      
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      const response = await fetch(url);
+      console.log(response)
+      if(!response){
+        
+      }
   }
 
   function displayWeatherData(weatherData){
