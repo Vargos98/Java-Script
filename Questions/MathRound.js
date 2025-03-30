105cb73652867c9027ae925be3bc8d62fb9f263d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-03-30 11:01:53 +0530

    Create MathRound.js

---
+++ Questions/MathRound.js
@@ -0,0 +1,13 @@
+let soup = 10;
+let burger =8;
+let iceCream = 5;
+
+
+// let totalCost = (soup+(3*burger)+iceCream);
+
+// console.log(totalCost)
+
+
+
+let items  = (18.99+(2*4.55)+6.89+7.89);
+console.log(Math.round(items))
